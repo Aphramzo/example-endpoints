d1fe8a0262f0c5275765389532bb74789492a53b
Tyler Walborn <tyler.walborn@sunrun.com>
2022-05-12 21:09:32 -0600

    Adding fake get user endpoint (#5)
    * Adding fake get user endpoint
    
    * Fixing roles
    
    * Expanding our login options

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,19 +1,78 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import {
+  APIGatewayEvent,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 import Debug from "debug";
+import { v4 } from "uuid";
 import { asyncHandlerWithStatus } from "../utils/utils";
+import jwt from "jsonwebtoken";
+import Fakerator from "fakerator";
+import internal from "stream";
+
 const logger = Debug("handler");
+const fakerator = Fakerator();
+
+type Role = {
+  id: string;
+  name: string;
+};
+
+type User = {
+  firstName: string;
+  lastName: string;
+  address: any; // So sue me
+  roles: Array<Role>;
+};
+
+function generateRole(): Role {
+  return { id: v4(), name: fakerator.lorem.word() };
+}
 
 async function asyncLogin(
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> {
-  logger("in asyncLogin handler");
+  const body = JSON.parse(event.body || "");
+  // just some stupid basic options for testing different responses
+  if (!body.password || !body.username) {
+    return {
+      statusCode: 400,
+      body: "Invalid Request. username and password required",
+    };
+  }
+
+  if (body.password === "password") {
+    return { statusCode: 401, body: "Invalid" };
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      firstName: "Faker",
-      lastName: "Tester",
+      userId: v4(),
+      token: jwt.sign({ fake: "data" }, "alsofakedata"),
     }),
+  };
+}
+
+async function asyncGetUser(
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> {
+  logger("in getUser");
+
+  const user: User = {
+    firstName: fakerator.names.firstName(),
+    lastName: fakerator.names.lastName(),
+    address: fakerator.entity.address(),
+    roles: [],
+  };
+
+  for (var i = 0; i < fakerator.random.number(2, 15); i++) {
+    user.roles.push(generateRole());
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(user),
   };
 }
 
@@ -21,4 +80,10 @@ export async function login(
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> {
   return asyncHandlerWithStatus(event, asyncLogin);
+}
+
+export async function getUser(
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> {
+  return asyncHandlerWithStatus(event, asyncGetUser);
 }
